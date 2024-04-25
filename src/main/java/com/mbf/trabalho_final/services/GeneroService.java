0d555568d9dfa8d290cb30d06737c6d303e70516
Melissa <melissasouza.dev@gmail.com>
2024-04-25 09:02:30 -0300

    Atualização CRUD em Genero

--- src/main/java/com/mbf/trabalho_final/services/GeneroService.java
+++ src/main/java/com/mbf/trabalho_final/services/GeneroService.java
@@ -27,4 +27,27 @@ public class GeneroService {
         repository.save(genero);
         return "Gênero inserido com sucesso!";
     }
+
+    public String alterarGenero(long id_genero, Genero genero){
+        Optional<Genero> optionalgenero = repository.findById(id_genero);
+        if (optionalgenero.isEmpty()) {
+            return "genero não encontrado.";
+        }
+        Genero generoExistence = optionalgenero.get();
+        generoExistence.setId_genero(genero.getId_genero());
+        generoExistence.setNome(genero.getNome());
+
+        repository.save(generoExistence);
+        return "Genero alterado com sucesso!";
+    }
+
+    public String removerGenero(long id_genero){
+        Optional<Genero> optionalGenero = repository.findById(id_genero);
+
+        if (optionalGenero.isEmpty()) {
+            return "Genero não encontrado.";
+        }
+        repository.delete(optionalGenero.get());
+        return "Genero excluído com sucesso!";
+    }  
 }
